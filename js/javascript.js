a771ab0a7802d2017b98b062b8d436335577be7b
zewrick <delav.js@gmail.com>
2018-11-21 09:32:12 -0500

    Delete javascript.js

--- js/javascript.js
+++
@@ -1,6 +0,0 @@
-let iconMenu = document.getElementById('icon-menu');
-let nav = document.getElementById('nav');
-
-iconMenu.addEventListener('click', function() {
-   nav.classList.toggle('display');                       
-})
